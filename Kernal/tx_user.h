fce6536b4e67150b1e785be5a984ac87f162ba77
drinkto <z_one787@163.com>
2026-02-04 10:44:19 +0800

    📃 docs(tx_user): 汉化tx_user.h

--- Kernal/tx_user.h
+++ Kernal/tx_user.h
@@ -53,7 +53,7 @@
    here by commenting or un-commenting the conditional compilation defined OR supply the defines
    though the compiler's equivalent of the -D option.
 
-   For maximum speed, the following should be defined:
+   最快速度优化需要开启的选项:
 
         TX_MAX_PRIORITIES                       32
         TX_DISABLE_PREEMPTION_THRESHOLD
@@ -65,7 +65,7 @@
         TX_DISABLE_STACK_FILLING
         TX_INLINE_THREAD_RESUME_SUSPEND
 
-   For minimum size, the following should be defined:
+   最小代码优化需要开启的选项:
 
         TX_MAX_PRIORITIES                       32
         TX_DISABLE_PREEMPTION_THRESHOLD
@@ -81,9 +81,8 @@
    therefore return an error if this option is used. This may or may not be desirable for a
    given application.  */
 
-/* Override various options with default values already assigned in tx_port.h. Please also refer
-   to tx_port.h for descriptions on each of these options.  */
 
+/* 覆盖tx_port.h 里面的宏定义  */
 /*#define TX_MAX_PRIORITIES                32*/
 /*#define TX_THREAD_USER_EXTENSION                ????*/
 /*#define TX_TIMER_THREAD_STACK_SIZE                1024*/
@@ -91,53 +90,37 @@
 
 /*#define TX_MINIMUM_STACK                200*/
 
-/* Determine if timer expirations (application timers, timeouts, and tx_thread_sleep calls
-   should be processed within the a system timer thread or directly in the timer ISR.
-   By default, the timer thread is used. When the following is defined, the timer expiration
-   processing is done directly from the timer ISR, thereby eliminating the timer thread control
-   block, stack, and context switching to activate it.  */
 
-/*#define TX_TIMER_PROCESS_IN_ISR*/
+/*
+   确定定时器是否到期的处理，比如应用定时器，溢出时间和函数tx_thread_sleep调用等，是在系统定时器任务里面还是在定时器中断里面调用。
+   默认是在定时任务里面，当定义了下面函数后，将直接在定时器中断里面处理，可以去掉定时器任务所消耗资源。 */
+// #define TX_TIMER_PROCESS_IN_ISR
 
-/* Determine if in-line timer reactivation should be used within the timer expiration processing.
-   By default, this is disabled and a function call is used. When the following is defined,
-   reactivating is performed in-line resulting in faster timer processing but slightly larger
-   code size.  */
 
-/*#define TX_REACTIVATE_INLINE*/
+/* 用于设置定时器激活是否采用内联方式，默认此功能是关闭的。如果使能后，内联方式的执行速度快，但增加代码量 */
+// #define TX_REACTIVATE_INLINE
 
-/* Determine is stack filling is enabled. By default, ThreadX stack filling is enabled,
-   which places an 0xEF pattern in each byte of each thread's stack.  This is used by
-   debuggers with ThreadX-awareness and by the ThreadX run-time stack checking feature.  */
 
-/*#define TX_DISABLE_STACK_FILLING*/
+/* 用于设置是否关闭栈填充，默认情况下是使能的，所有任务的栈空间全部填充为0xEF，
+ * 带有ThreadX调试组件或者运行时栈检测会用到。
+ */
+// #define TX_DISABLE_STACK_FILLING
 
-/* Determine whether or not stack checking is enabled. By default, ThreadX stack checking is
-   disabled. When the following is defined, ThreadX thread stack checking is enabled.  If stack
-   checking is enabled (TX_ENABLE_STACK_CHECKING is defined), the TX_DISABLE_STACK_FILLING
-   define is negated, thereby forcing the stack fill which is necessary for the stack checking
-   logic.  */
 
-/*#define TX_ENABLE_STACK_CHECKING*/
+/* 用于使能栈检测，默认是关闭的。此选项使能后，而TX_DISABLE_STACK_FILLING没使能时，栈填充将开启，方便栈检测 */
+// #define TX_ENABLE_STACK_CHECKING
 
-/* Determine if preemption-threshold should be disabled. By default, preemption-threshold is
-   enabled. If the application does not use preemption-threshold, it may be disabled to reduce
-   code size and improve performance.  */
 
+/* 用于设置是否关闭抢占阀值，默认是开启的。如果应用程序不需要此功能，关闭后可以降低代码需求，提升性能 */
 #define TX_DISABLE_PREEMPTION_THRESHOLD
 
-/* Determine if global ThreadX variables should be cleared. If the compiler startup code clears
-   the .bss section prior to ThreadX running, the define can be used to eliminate unnecessary
-   clearing of ThreadX global variables.  */
 
-/*#define TX_DISABLE_REDUNDANT_CLEARING*/
+/* 用于设置是否清零ThreadX全局变量，如果编译器启动代码在ThreadX运行前清除了.bss段，那么可以关闭不必要的清零 */
+// #define TX_DISABLE_REDUNDANT_CLEARING
 
-/* Determine if no timer processing is required. This option will help eliminate the timer
-   processing when not needed. The user will also have to comment out the call to
-   tx_timer_interrupt, which is typically made from assembly language in
-   tx_initialize_low_level. Note: if TX_NO_TIMER is used, the define TX_TIMER_PROCESS_IN_ISR
-   must also be used.  */
 
+/* 确定是否不需要定时器组，禁止后需要用户注释掉tx_initialize_low_level文件里面tx_timer_interrupt的调用。
+   另外，禁止后，必须使能TX_TIMER_PROCESS_IN_ISR */
 /*
 #define TX_NO_TIMER
 #ifndef TX_TIMER_PROCESS_IN_ISR
@@ -145,106 +128,91 @@
 #endif
 */
 
-/* Determine if the notify callback option should be disabled. By default, notify callbacks are
-   enabled. If the application does not use notify callbacks, they may be disabled to reduce
-   code size and improve performance.  */
 
+/* 用于设置是否关闭通知回调，默认是使能的。如果应用程序没有用到消息回调，关闭掉后可以减小代码，并且可以提升性能。 */
 #define TX_DISABLE_NOTIFY_CALLBACKS
 
-/* Determine if the tx_thread_resume and tx_thread_suspend services should have their internal
-   code in-line. This results in a larger image, but improves the performance of the thread
-   resume and suspend services.  */
 
-/*#define TX_INLINE_THREAD_RESUME_SUSPEND*/
+/* 使能tx_thread_resume和tx_thread_suspend使用内联代码，优势是提升这两个函数的执行性能，劣势是增加代码量 */
+// #define TX_INLINE_THREAD_RESUME_SUSPEND
 
-/* Determine if the internal ThreadX code is non-interruptable. This results in smaller code
-   size and less processing overhead, but increases the interrupt lockout time.  */
 
-/*#define TX_NOT_INTERRUPTABLE*/
+/* 设置TreadX内核不可中断，好处是降低处理负担，并且产生的代码小。但增加锁时间 */
+// #define TX_NOT_INTERRUPTABLE
 
-/* Determine if the trace event logging code should be enabled. This causes slight increases in
-   code size and overhead, but provides the ability to generate system trace information which
-   is available for viewing in TraceX.  */
 
-/*#define TX_ENABLE_EVENT_TRACE*/
+/* 使能事件Trace，会稍微增加点代码 */
+// #define TX_ENABLE_EVENT_TRACE
 
-/* Determine if block pool performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various block pool performance information. */
 
-/*#define TX_BLOCK_POOL_ENABLE_PERFORMANCE_INFO*/
+/* 使能BLOCK_POOL信息获取 */
+// #define TX_BLOCK_POOL_ENABLE_PERFORMANCE_INFO
 
-/* Determine if byte pool performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various byte pool performance information. */
 
-/*#define TX_BYTE_POOL_ENABLE_PERFORMANCE_INFO*/
+/* 使能BYTE_POOL信息获取 */
+// #define TX_BYTE_POOL_ENABLE_PERFORMANCE_INFO
 
-/* Determine if event flags performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various event flags performance information. */
 
-/*#define TX_EVENT_FLAGS_ENABLE_PERFORMANCE_INFO*/
+/* 使能事件标志信息获取 */
+// #define TX_EVENT_FLAGS_ENABLE_PERFORMANCE_INFO
 
-/* Determine if mutex performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various mutex performance information. */
 
-/*#define TX_MUTEX_ENABLE_PERFORMANCE_INFO*/
+/* 使能互斥信号量信息获取 */
+// #define TX_MUTEX_ENABLE_PERFORMANCE_INFO
 
-/* Determine if queue performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various queue performance information. */
 
-/*#define TX_QUEUE_ENABLE_PERFORMANCE_INFO*/
+/* 使能消息对象信息获取 */
+// #define TX_QUEUE_ENABLE_PERFORMANCE_INFO
 
-/* Determine if semaphore performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various semaphore performance information. */
+/* 使能信号量信息获取  */
+// #define TX_SEMAPHORE_ENABLE_PERFORMANCE_INFO
 
-/*#define TX_SEMAPHORE_ENABLE_PERFORMANCE_INFO*/
 
-/* Determine if thread performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various thread performance information. */
+/* 使能任务性能信息获取 */
+// #define TX_THREAD_ENABLE_PERFORMANCE_INFO
 
-/*#define TX_THREAD_ENABLE_PERFORMANCE_INFO*/
 
-/* Determine if timer performance gathering is required by the application. When the following is
-   defined, ThreadX gathers various timer performance information. */
+/* 使能定时器性能信息获取 */
+// #define TX_TIMER_ENABLE_PERFORMANCE_INFO
 
-/*#define TX_TIMER_ENABLE_PERFORMANCE_INFO*/
 
-/* Define if the execution change notify is enabled. */
+/* 使能执行变更通知 */
+// #define TX_ENABLE_EXECUTION_CHANGE_NOTIFY
 
-/*#define TX_ENABLE_EXECUTION_CHANGE_NOTIFY*/
 
-/* Define the get system state macro. */
-
+/* 定义获取系统状态的宏。*/
 /*#define TX_THREAD_GET_SYSTEM_STATE() _tx_thread_system_state */
 
-/* Define the check for whether or not to call the
-    _tx_thread_system_return function (TX_THREAD_SYSTEM_RETURN_CHECK(c)). */
 
+/* 定义用于判断是否调用 _tx_thread_system_return 函数的条件
+                                        (TX_THREAD_SYSTEM_RETURN_CHECK(c))*/
 /*#define #define TX_THREAD_SYSTEM_RETURN_CHECK (c)  ((ULONG) _tx_thread_preempt_disable)*/
 
-/* Define the common timer tick reference for use by other middleware components. */
 
+/* 定义通用的定时器滴答时长基准值，以供其他中间件组件使用。*/
 /*#define TX_TIMER_TICKS_PER_SECOND                100*/
 
-/* Determinate if the basic alignment type is defined. */
 
-/*#define ALIGN_TYPE_DEFINED*/
+/* 确定基本对齐类型是否已定义。*/
+// #define ALIGN_TYPE_DEFINED
 
-/* Define basic alignment type used in block and byte pool operations. */
 
-/*#define ALIGN_TYPE  ULONG*/
+/* 定义在块和字节池操作中使用的基本对齐类型 */
+// #define ALIGN_TYPE  ULONG
 
-/* Define the TX_MEMSET macro to the standard library function. */
 
-/*#define TX_MEMSET  memset((a),(b),(c))*/
+/* 将 TX_MEMSET 宏定义为标准库函数。*/
+// #define TX_MEMSET  memset((a),(b),(c))
+
 
 #ifdef __IAR_SYSTEMS_ASM__
 /* Define if the IAR library is supported. */
 /*#define TX_ENABLE_IAR_LIBRARY_SUPPORT*/
 #endif
 
-/* Define if the safety critical configuration is enabled. */
 
-/*#define TX_SAFETY_CRITICAL*/
+/* 使能安全关键型配置 */
+// #define TX_SAFETY_CRITICAL
 
 #endif
 
